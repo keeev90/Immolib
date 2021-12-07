2062cb1b628db109ae1dce76ef1c33b35beef1f6
Beygs <boris.gilles@hotmail.fr>
2021-12-07 19:24:00 +0100

    navbar btn

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,12 +1,32 @@
 const connexionBtn = document.querySelector(".connexion-btn");
 const connexionMenu = document.querySelector(".navbar__items__connexion__menu");
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
 
-connexionBtn.addEventListener("click", function() {
-  connexionBtn.classList.toggle("active");
-  connexionMenu.classList.toggle("active");
+
+if (connexionBtn) {
+  connexionBtn.addEventListener("click", function() {
+    connexionBtn.classList.toggle("active");
+    connexionMenu.classList.toggle("active");
+  });
+
+  connexionMenu.addEventListener('mouseleave', function() {
+    connexionMenu.classList.remove("active");
+    connexionBtn.classList.remove("active");
+  });
+}
+
+hamburgerBtn.addEventListener('click', () => {
+  hamburgerBtn.classList.toggle('is-active');
+  hamburgerMenu.classList.toggle('is-active');
+  console.log('coucou')
 });
 
-connexionMenu.addEventListener('mouseleave', function() {
-  connexionMenu.classList.remove("active");
-  connexionBtn.classList.remove("active");
+window.addEventListener('scroll', () => {
+  const scroll = Math.round(window.scrollY || window.pageYOffset)
+  if (scroll > 100) {
+    hamburgerBtn.style.display = 'block';
+  } else if (scroll < 100) {
+    hamburgerBtn.style.display = 'none';
+  }
 });
