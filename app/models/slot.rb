64c88d3329d5764cec67aa5a19830fd29a27c8d1
keeev90 <kvn.leman@gmail.com>
2021-12-07 11:42:11 +0100

    add routes and dependent destroy

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,7 +1,7 @@
 class Slot < ApplicationRecord
   #Associations
   belongs_to :property
-  has_many :appointments
+  has_many :appointments, dependent: :destroy
   has_many :candidates, through: :appointment
 
 end
