8f8fd23bbb341b4458848384cc1f81f1bce2a446
Beygs <boris.gilles@hotmail.fr>
2021-12-07 10:21:10 +0100

    autocomplete ugly color fixed

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -4,9 +4,9 @@ const connexionMenu = document.querySelector(".navbar__items__connexion__menu");
 connexionBtn.addEventListener("click", function() {
   connexionBtn.classList.toggle("active");
   connexionMenu.classList.toggle("active");
-})
+});
 
 connexionMenu.addEventListener('mouseleave', function() {
   connexionMenu.classList.remove("active");
   connexionBtn.classList.remove("active");
-})
+});
