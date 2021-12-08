c84fe3f6171917a5786422ec9db43a06323589cd
keeev90 <kvn.leman@gmail.com>
2021-12-08 23:32:56 +0100

    ok

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,7 @@
 class Property < ApplicationRecord
+  #Callbacks
+  after_create :update_property_id
+
   #Associations
   belongs_to :owner, class_name: "User"
   has_many :slots, dependent: :destroy
@@ -8,4 +11,16 @@ class Property < ApplicationRecord
   
   #Validations
   validates :title, presence: true, length: { in: 3..140, message: "Le nombre de caractères doit être compris entre 3 et 140" }
+
+  def go_visit_url
+    @id = self.id
+    return "https://immolib-dev.herokuapp.com/properties/#{@id}/go-visit"
+  end
+
+  private
+
+  def update_property_id
+    self.id = 5.times.map { rand(1..9) }.join.to_i
+  end
+
 end
