2e4a7bb0a49114dc77fa39052203c00ec51a9e80
Cedric Le Net <lenetcedric@gmail.com>
2021-12-08 14:30:02 +0100

    Creation of Appointment Controleur

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'appointments/index'
+  get 'appointments/new'
+  get 'appointments/create'
+  get 'appointments/show'
+  get 'appointments/edit'
+  get 'appointments/update'
+  get 'appointments/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
