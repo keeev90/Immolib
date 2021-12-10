7a4ed370c6cfbe89da9f90e927e2c73f695ddf50
Beygs <boris.gilles@hotmail.fr>
2021-12-10 14:21:02 +0100

    navbar responsive

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,20 +1,20 @@
-const connexionBtn = document.querySelector(".connexion-btn");
-const connexionMenu = document.querySelector(".navbar__items__connexion__menu");
+const connectionBtns = document.querySelectorAll(".connection-btn");
 const hamburgerBtn = document.querySelector('.hamburger-btn');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 
+connectionBtns.forEach(btn => {
+  const connectionMenu = btn.parentNode.querySelector('.navbar__items__connection__menu');
 
-if (connexionBtn) {
-  connexionBtn.addEventListener("click", function() {
-    connexionBtn.classList.toggle("active");
-    connexionMenu.classList.toggle("active");
+  btn.addEventListener("click", function() {
+    btn.classList.toggle("active");
+    connectionMenu.classList.toggle("active");
   });
 
-  connexionMenu.addEventListener('mouseleave', function() {
-    connexionMenu.classList.remove("active");
-    connexionBtn.classList.remove("active");
+  connectionMenu.addEventListener('mouseleave', function() {
+    connectionMenu.classList.remove("active");
+    btn.classList.remove("active");
   });
-}
+});
 
 hamburgerBtn.addEventListener('click', () => {
   hamburgerBtn.classList.toggle('is-active');
