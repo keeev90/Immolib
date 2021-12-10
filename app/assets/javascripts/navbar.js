b30a5c2980fcae047e74558fad45ceddbf1e0b1d
Beygs <boris.gilles@hotmail.fr>
2021-12-10 13:44:36 +0100

    navbar responsive

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -31,7 +31,15 @@ window.addEventListener('scroll', () => {
   const scroll = Math.round(window.scrollY || window.pageYOffset)
   if (scroll > 100) {
     hamburgerBtn.style.display = 'block';
-  } else if (scroll < 100) {
+  } else if (scroll < 100 && window.innerWidth > 800) {
     hamburgerBtn.style.display = 'none';
   }
 });
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 800) {
+    hamburgerBtn.style.display = 'block'
+  } else {
+    hamburgerBtn.style.display = 'none'
+  }
+})
