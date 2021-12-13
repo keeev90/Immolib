60eb7cc367884c523b112755d06a42a36f8dbd63
Beygs <boris.gilles@hotmail.fr>
2021-12-13 15:58:06 +0100

    google omniauth done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   #Associations
   has_many :properties, foreign_key: 'owner_id', dependent: :destroy
@@ -11,4 +12,17 @@ class User < ApplicationRecord
   
   #Validations
   # validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Merci de renseigner une adresse email valide." }
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    unless user
+      user = User.create(
+        email: data['email'],
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+    user
+  end
 end
