78f83de41d61b3530db4c0030fc99c6c0ff087bb
keeev90 <kvn.leman@gmail.com>
2021-12-13 15:55:38 +0100

    fix new slot validate methods

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -6,14 +6,13 @@ class Slot < ApplicationRecord
   has_many :candidates, through: :appointments
   
   #Validations
-  #validate :start_date_cannot_be_in_the_past
-  #validate :overlaps_with_other?
+  validate :start_date_cannot_be_in_the_past
+  validate :overlaps_with_other?
 
   validates :start_date, presence: true
   validates :duration, presence: true
   validates :max_appointments, presence: true, numericality: { only_integer: true }
 
-  # useful methods now or later
 
   def end_date
     start_date + duration.minutes
