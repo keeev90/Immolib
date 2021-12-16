a5d248b6004696e750a7d387353eeb56e43be28d
keeev90 <kvn.leman@gmail.com>
2021-12-16 17:44:46 +0100

    mailers desactivated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   #Callbacks
-  after_create :welcome_send
+  #after_create :welcome_send
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
