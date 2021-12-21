da4f1601f50b4829214222ed739ed21d1831fdaa
keeev90 <kvn.leman@gmail.com>
2021-12-21 20:13:59 +0100

    send validation email after payment success

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,35 +3,35 @@ class UserMailer < ApplicationMailer
  
   def welcome_email(user)
     @user = user 
-    @subtitle = "Bienvenue 👋"
+    @subtitle = "Merci pour votre inscription"
     mail(to: @user.email, subject: "Bienvenue sur immolib 👋") 
   end
 
   def new_property_validation_email(property)
     @property = property
     @user = property.owner
-    @subtitle = "Confirmation de la création de votre logement immolib 🎉"
-    mail(to: @user.email, subject: "Confirmation de la création de votre logement immolib 🎉")
+    @subtitle = "Merci pour votre confiance"
+    mail(to: @user.email, subject: "Votre logement immolib a bien été créé 🎉")
   end
 
   def new_appointment_validation_email(appointment)
     @appointment = appointment
     @user = appointment.candidate
-    @subtitle = "Confirmation de votre visite 🎉"
-    mail(to: @user.email, subject: "Confirmation de votre visite 🎉")
+    @subtitle = "Rendez-vous sur votre espace immolib pour compléter vos informations"
+    mail(to: @user.email, subject: "Votre candidature est bien enregistrée 🎉")
   end
 
   def candidate_folder_reminder_email(appointment)
     @appointment = appointment
     @user = appointment.candidate
-    @subtitle = "Votre dossier de location est incomplet 📋"
+    @subtitle = "Rendez-vous sur votre espace immolib pour finaliser votre dossier"
     mail(to: @user.email, subject: "Votre dossier de location est incomplet 📋")
   end
 
   def appointment_reminder_email(appointment)
     @appointment = appointment
     @user = appointment.candidate
-    @subtitle = "J-1 avant votre visite ⏰"
+    @subtitle = "Consultez les instructions logistiques pour votre visite"
     mail(to: @user.email, subject: "J-1 avant votre visite ⏰")
   end
 
