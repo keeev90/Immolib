e3c92e177fd005b78ae1ef00c0caa594d0826e8d
keeev90 <kvn.leman@gmail.com>
2022-01-21 17:39:08 +0100

    updates new candidate

--- app/controllers/new_candidates_controller.rb
+++ app/controllers/new_candidates_controller.rb
@@ -3,12 +3,16 @@ class NewCandidatesController < ApplicationController
   
   def welcome_candidate
     @property = Property.find(params[:id])
+    if @property.already_has_appointment?(current_user) == true
+      flash[:warning] = "Vous avez déjà répondu à cette annonce. Merci de gérer votre candidature depuis votre espace immolib 🙏"
+      redirect_to user_path(current_user)
+    end
   end
 
   def step1_login #book appointement (if login)
     @property = Property.find(params[:id])
-    @slots = @property.slots
     @new_candidate = true
+    @slots = @property.slots
     @date_arr = ["", "jan.", "fév.", "mar.", "avr.", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."]
 
     respond_to do |format|
